3ba18ec4b507b3b6fcbc49bba5a503baba051c0a
Ed Finkler <coj@funkatron.com>
2014-02-13 11:19:46 -0500

    add example of Resty::postJson()

--- example.php
+++ example.php
@@ -18,3 +18,20 @@ var_dump($resp['body']);
 
 echo "\n\$resp['body_raw']:\n";
 var_dump($resp['body_raw']);
+
+
+/**
+ * Resty::postJson() encodes the object or array as JSON and sends it as 
+ * the request body.
+ */
+$to_json = array(
+                 "foo"=>array(
+                              "bar"=>"baz",
+                              "bee"=>"bim",
+                              "bop"=>23
+                              )
+                 );
+$resp = $resty->postJson('post', $to_json);
+
+echo "\n\$resp['body']->json:\n";
+var_dump($resp['body']->json);
